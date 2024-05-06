2b74339145d9df9100741c3f9a3efb849fb43002
abhishek98s <abhishekshakya98.com@gmail.com>
2024-05-06 14:27:29 +0545

    added mseeage

--- src/utils/error-handler.ts
+++ src/utils/error-handler.ts
@@ -1,11 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
-import { authExceptionMessage } from '../auth/constant/authExceptionMessage';
 
 export const errorHandlerMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
     return res.status(500).json(
         {
-            msg: authExceptionMessage.INTERNAL_SERVER_ERRROR,
-            error: err.message,
+            success: false,
+            message: err.message,
         },
     );
 };

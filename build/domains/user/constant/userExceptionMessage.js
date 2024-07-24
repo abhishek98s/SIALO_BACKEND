9851dd016dac0d7223a2a52a471043a056ca82e4
abhishek98s <abhishekshakya98.com@gmail.com>
2024-07-24 11:54:17 +0545

    remove: log and buildg

--- build/domains/user/constant/userExceptionMessage.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userExceptionMessage = void 0;
-exports.userExceptionMessage = {
-    INVALID_ID: 'Invalide Id',
-    USER_NOT_FOUND: 'User doesn\'t exit',
-};

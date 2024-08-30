e62bf891f74d56d614e133f58e05ebdf3c7028e3
abhishek98s <abhishekshakya98.com@gmail.com>
2024-08-31 00:19:01 +0545

    feat: endpoint to fetch the friends

--- src/domains/user/user.routes.ts
+++ src/domains/user/user.routes.ts
@@ -7,12 +7,12 @@ const router = express.Router();
 
 router.use(verifyToken);
 
+router.get('/friends', user_controller.getFriends);
 router.get('/friendRequests', user_controller.getFriendRequests);
 router.get('/search', user_controller.searchUser);
 router.get('/recommendation', user_controller.fetchUnknownPeople);
 router.route('/:id').get(user_controller.getUser).delete(user_controller.deleteUser);
 router.get('/', user_controller.fetchAll);
-
 
 router.patch('/friend/add/:friendId', user_controller.addFriend)
     .patch('/friend/accept/:friendId', user_controller.acceptRequest)

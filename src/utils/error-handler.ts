c0218d86cc6020e16ffd15a82c42ce07a42aa2ba
abhishek98s <abhishekshakya98.com@gmail.com>
2024-09-11 10:10:37 +0545

    feat: erro status code

--- src/utils/error-handler.ts
+++ src/utils/error-handler.ts
@@ -1,7 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
+import { HttpError } from 'http-errors';
 
-export const errorHandlerMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    return res.status(500).json(
+export const errorHandlerMiddleware = (err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = err.status || 500;
+
+    return res.status(statusCode).json(
         {
             status: false,
             message: err.message,

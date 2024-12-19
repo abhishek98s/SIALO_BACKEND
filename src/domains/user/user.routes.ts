c2576db28f23bc9ac22d4ae2c0ba8c0e3b231f31
abhishek98s <abhishekshakya98.com@gmail.com>
2024-12-19 17:38:37 +0545

    fixed: comment image

--- src/domains/user/user.routes.ts
+++ src/domains/user/user.routes.ts
@@ -14,13 +14,15 @@ router.get('/friends/:userId', user_controller.getFriends);
 router.get('/friendRequests', user_controller.getFriendRequests);
 router.get('/search', user_controller.searchUser);
 router.get('/recommendation', user_controller.fetchUnknownPeople);
-router.route('/:id').get(user_controller.getUser)
-    .delete(user_controller.deleteUser)
-    .patch(joiValidationMiddleware(changeUsernameSchema), user_controller.updateUsername);
-router.get('/', user_controller.fetchAll);
 
 router.patch('/profilePicture', upload.single('sialo_profile_image'), joiFileValidationMiddleware(fileSchema), verifyToken, user_controller.updateProfilePicture);
 router.patch('/coverPicture', upload.single('sialo_cover_image'), joiFileValidationMiddleware(fileSchema), verifyToken, user_controller.updateCoverPicture);
+
+router.route('/:id').get(user_controller.getUser)
+    .delete(user_controller.deleteUser)
+    .patch(joiValidationMiddleware(changeUsernameSchema), user_controller.updateUsername);
+
+    router.get('/', user_controller.fetchAll);
 
 router.patch('/friend/add/:friendId', user_controller.addFriend)
     .patch('/friend/accept/:friendId', user_controller.acceptRequest)

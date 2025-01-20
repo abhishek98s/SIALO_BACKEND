811a53bd4bdae7f4dd4c254450d5bcd7569a1ced
abhishek98s <abhishekshakya98.com@gmail.com>
2025-01-20 12:34:13 +0545

    fix: cron disabled

--- src/cron/story.crone.ts
+++ src/cron/story.crone.ts
@@ -1,13 +1,13 @@
-import cron from 'node-cron';
+// import cron from 'node-cron';
 import { deleteOldStories } from '../domains/story/story.repository';
 
-export const cron_story = () => {
-    cron.schedule('* * * * *', async () => {
+export const cron_story = async() => {
+    // cron.schedule('* * * * *', async () => {
         try {
             await deleteOldStories();
             console.log('Cron runned');
         } catch (error) {
             console.error('Error deleting old posts:', error);
         }
-    });
+    // });
 };

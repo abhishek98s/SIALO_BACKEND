a6b5c04b60fe46d6b5b829e91dc79a33068e8194
abhishek98s <abhishekshakya98.com@gmail.com>
2025-04-25 12:47:21 +0545

    fix: validation messages

--- src/domains/user/constant/userSuccessMessage.ts
+++ src/domains/user/constant/userSuccessMessage.ts
@@ -1,3 +1,12 @@
-export const userSuccessnMessage = {
-    USERNAME_UPDATE_SUCCESS: 'Username updated',
+export const userSuccessMessages = {
+  USERNAME_UPDATE_SUCCESS: 'Username updated',
+  FRIEND_ADDED: 'Friend added',
+  FRIEND_REMOVED: 'Friend removed',
+  ACCOUNT_DELETED: 'Account deleted',
+  PROFILE_PIC_UPDATED: 'Profile picture updated',
+  COVER_PIC_UPDATED: 'Cover picture updated',
+  FRIEND_REQUEST_SENT: 'Friend request sent',
+  FRIEND_REQUEST_ACCEPTED: 'Friend request accepted',
+  FRIEND_REQUEST_REJECTED: 'Friend request rejected',
+  FRIEND_REQUEST_CANCELLED: 'Friend request cancelled',
 };

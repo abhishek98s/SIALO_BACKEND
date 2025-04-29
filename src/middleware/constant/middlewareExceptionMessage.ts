3f1543949b9b88a3b4b139903c29041c7cd1fb34
abhishek98s <abhishekshakya98.com@gmail.com>
2025-04-29 14:31:27 +0545

    fix: error message

--- src/middleware/constant/middlewareExceptionMessage.ts
+++ src/middleware/constant/middlewareExceptionMessage.ts
@@ -6,4 +6,5 @@ export const middlewareExceptionMessage = {
   INTERNAL_SERVER_ERROR: 'Internal Server Error',
   FILE_REQUIRED: 'File is not provided',
   CONTENT_TYPE_INVALID: 'Content type must be multipart/form-data',
+  FIELD_NAME_INCORRECT: 'Please provide a file with correct field name',
 };
